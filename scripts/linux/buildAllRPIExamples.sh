4efdb430f362a9a290f793b259e12b189ced1310
Autr <gilbert.sinnott@gmail.com>
2019-02-11 22:57:48 +0100

    Failsafe for non-directories, list successfully compiled and failed; copy all to of-rpi-examples folder

--- scripts/linux/buildAllRPIExamples.sh
+++ scripts/linux/buildAllRPIExamples.sh
@@ -1,29 +1,47 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PARENT_DIR="$(dirname "$DIR")"
-MAKEFILE_PATH=$PARENT_DIR/templates/linuxarmv6l/Makefile
-cd ${DIR}/../../examples
+#!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+PARENT_DIR="$(dirname "$DIR")" 
+ROOT_DIR=${DIR}/../..
+COMPILE_DIR=${ROOT_DIR}/of-rpi-examples
+MAKEFILE_PATH=$PARENT_DIR/templates/linuxarmv6l/Makefile 
+mkdir ${ROOT_DIR}/of-rpi-examples
+cd ${DIR}/../../examples
+success_list=()
+error_list=()
 for category in $(ls -1d *)
 do
-        if [ $category != "addons" ] && [ $category != "ios" ] && [ $category != "android" ]; then
- 
+        if [ $category != "addons" ] && [ $category != "ios" ] && [ $category != "android" ] && [ -d "$category" ]; then
+
                 echo "CHANGED TO CATEGORY >"+$category
+				mkdir $COMPILE_DIR/$category
                 cd $category
                 for j in $(ls -1d *)
                 do
                         echo ">>$j"
-                        cd $j
-#                       make clean -f $MAKEFILE_PATH
-                        make -f $MAKEFILE_PATH
-                        ret=$?
-                        if [ $ret -ne 0 ];
-                        then
-                                echo "error compiling: " + $j
-                        else
-                                echo "successfully compiled :" + $j
-                        fi
-                        cd ../
+			if [ -d $j ]; then
+                        	cd $j
+#							read -p "Press any key to compile next example, $category $j :"
+#                        	make clean -f $MAKEFILE_PATH
+                        	make -f $MAKEFILE_PATH 
+                        	ret=$?
+                        	if [ $ret -ne 0 ];
+                        	then
+					error_list+=($j)
+                                	echo "error compiling: " + $j
+                        	else
+					success_list+=($j)
+                                	echo "successfully compiled :" + $j
+					cp bin/$j $COMPILE_DIR/$category
+                        	fi
+                        	cd ../
+			fi
                 done
                 cd ../
         fi
 done
+
+echo "Failing builds"
+echo ${error_list[*]}
+echo "Successfully built"
+echo ${success_list[*]}
